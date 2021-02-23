034275e07276f7d4861fc90f6e973e2df5e29e6e
[Jacob White <ec2-user@ip-172-31-43-1.ec2.internal>
2021-02-23 02:51:48 +0000

    added in socket io, and use effect, now changes can be sent to another user and will see the correct X being marked when another user clicks

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json, session
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='./build/static')
 
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    json=json,
+    manage_session=False
+)
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+@socketio.on('connect')
+def on_connect():
+    print('User connected!')
 
-app.run(
+@socketio.on('disconnect')
+def on_disconnect():
+    print('User disconnected!')
+
+@socketio.on('move')
+def on_move(data): 
+    print(str(data))
+    socketio.emit('move',  data, broadcast=True, include_self=False)
+
+socketio.run(
+    app,
     host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
 )
--- src/Board.js
+++ src/Board.js
@@ -1,16 +1,29 @@
 import { Box } from './Box.js';
 import React, { useEffect, useState } from "react";
+import io from 'socket.io-client';
 
+const socket = io();
 
 export function Board(props) {
     const initialState = Array(9).fill(null);
     const [box, setBox] = useState(initialState);
     
+    
+    
     function handleClick(i) {
         const newBox = [...box];
         newBox[i] = 'X';
         setBox(newBox);
+        socket.emit('move', {i: i});
     }
+    
+    useEffect(() => {
+    socket.on('move', (data) => {
+        console.log(data);
+        console.log(Box[data.i]);
+        setBox(prevBox => [...prevBox, prevBox[data.i] = 'X']);
+    });
+  }, []);
     
     function renderBox(i) {
     return <Box value={box[i]} onClick= {() => handleClick(i)} />;
