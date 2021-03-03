8f80dd11e88b5f800a82fa322e9434cd8fd1a8a6
[Jacob White <ec2-user@ip-172-31-43-1.ec2.internal>
2021-03-03 19:29:11 +0000

    Merged from milestone_1 branch

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ app = Flask(__name__, static_folder='./build/static')
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-<<<<<<< HEAD
-=======
 userList = []
 counter = 0
 users = []
 
->>>>>>> milestone_1
 socketio = SocketIO(
     app,
     cors_allowed_origins="*",
@@ -26,31 +23,11 @@ socketio = SocketIO(
 def index(filename):
     return send_from_directory('./build', filename)
 
-<<<<<<< HEAD
-# When a client connects from this Socket connection, this function is run
-=======
->>>>>>> milestone_1
+
 @socketio.on('connect')
 def on_connect():
     print('User connected!')
 
-<<<<<<< HEAD
-# When a client disconnects from this Socket connection, this function is run
-@socketio.on('disconnect')
-def on_disconnect():
-    print('User disconnected!')
-
-# When a client emits the event 'chat' to the server, this function is run
-# 'chat' is a custom event name that we just decided
-@socketio.on('chat')
-def on_chat(data): # data is whatever arg you pass in your emit call on client
-    print(str(data))
-    # This emits the 'chat' event from the server to all clients except for
-    # the client that emmitted the event that triggered this function
-    socketio.emit('chat',  data, broadcast=True, include_self=False)
-
-# Note that we don't call app.run anymore. We call socketio.run with app arg
-=======
 @socketio.on('disconnect')
 def on_disconnect():
     print('User disconnected!')
@@ -87,7 +64,6 @@ def on_move(data):
     print(str(data))
     socketio.emit('move',  data, broadcast=True, include_self=False)
 
->>>>>>> milestone_1
 socketio.run(
     app,
     host=os.getenv('IP', '0.0.0.0'),
