8f060e6129b4911c59ebb3e7757452b71d73bada
Jacob White <jjw37@njit.edu>
2021-03-13 15:49:55 +0000

    Used  pylint on .py files and formated with yapf

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""Server side code"""
 import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
@@ -25,40 +26,43 @@ DB = SQLAlchemy(APP)
 
 import models
 
-SOCKETIO = SocketIO(
-    APP,
-    cors_allowed_origins="*",
-    json=json,
-    manage_session=False
-)
+SOCKETIO = SocketIO(APP,
+                    cors_allowed_origins="*",
+                    json=json,
+                    manage_session=False)
+
 
 @APP.route('/', defaults={"filename": "index.html"})
 @APP.route('/<path:filename>')
 def index(filename):
+    """Sending file"""
     return send_from_directory('./build', filename)
 
 
 @SOCKETIO.on('connect')
 def on_connect():
+    """socket event for user connect"""
     print('User connected!')
+
 
 @SOCKETIO.on('disconnect')
 def on_disconnect():
+    """socket event for user disconnect"""
     print('User disconnected!')
-    
-    
 
 
 @SOCKETIO.on('login')
 def on_login(data):
+    """returns current userlist and list of users from database with any new users added"""
     print(str(data))
     global USER_LIST
     global COUNTER
     global LEADER_BOARD
     leader_board_name = []
     leader_board_score = []
-    try :
-        db_query = models.Player.query.order_by(models.Player.score.desc()).all()
+    try:
+        db_query = models.Player.query.order_by(
+            models.Player.score.desc()).all()
         for row in db_query:
             LEADER_BOARD[row.username] = row.score
         if data['userName'] not in LEADER_BOARD:
@@ -88,12 +92,21 @@ def on_login(data):
         USERS.append("spectator " + data['userName'])
     print(USER_LIST)
     print(COUNTER)
-    SOCKETIO.emit('login', {'users': USERS, 'leaderBoardName': leader_board_name, 'leaderBoardScore': leader_board_score})
-    SOCKETIO.emit('user_count', {'counter': COUNTER}, broadcast=False, include_self=True)
+    SOCKETIO.emit(
+        'login', {
+            'users': USERS,
+            'leaderBoardName': leader_board_name,
+            'leaderBoardScore': leader_board_score
+        })
+    SOCKETIO.emit('user_count', {'counter': COUNTER},
+                  broadcast=False,
+                  include_self=True)
+
 
 @SOCKETIO.on('winner')
 def on_winner(data):
-   
+    """socket event for winner, updates database of winner/loser score, returns updated list"""
+
     board = {}
     board_name = []
     board_score = []
@@ -105,25 +118,28 @@ def on_winner(data):
     global USER_LIST
     player_list = USER_LIST
     winner_player = data['xNext']
-    if winner_player == True:
+    if winner_player:
         print('winner is ' + player_list[1])
         winner = player_list[1]
         print('loser is ' + player_list[0])
         loser = player_list[0]
-    if winner_player == False:
+    if not winner_player:
         print('winner is ' + player_list[0])
         winner = player_list[0]
         print('loser is ' + player_list[1])
         loser = player_list[1]
     print("steve")
     try:
-        winner_qr = DB.session.query(models.Player).filter_by(username = winner).first()
+        winner_qr = DB.session.query(
+            models.Player).filter_by(username=winner).first()
         winner_qr.score = winner_qr.score + 1
         DB.session.commit()
-        loser_qr = DB.session.query(models.Player).filter_by(username = loser).first()
+        loser_qr = DB.session.query(
+            models.Player).filter_by(username=loser).first()
         loser_qr.score = loser_qr.score - 1
         DB.session.commit()
-        db_query = models.Player.query.order_by(models.Player.score.desc()).all()
+        db_query = models.Player.query.order_by(
+            models.Player.score.desc()).all()
         for row in db_query:
             board[row.username] = row.score
         for name in board:
@@ -133,23 +149,31 @@ def on_winner(data):
         board_score = [12]
         board_name = ['bill']
     print(board)
-    SOCKETIO.emit('updateBoard', {'boardName': board_name, 'boardScore': board_score})
+    SOCKETIO.emit('updateBoard', {
+        'boardName': board_name,
+        'boardScore': board_score
+    })
+
 
 @SOCKETIO.on('restart')
 def on_restart(data):
+    """socket event for restart, resets the board of all players/spectators"""
     global WINNER_COUNT
     WINNER_COUNT = 0
     print("The winner count is " + str(WINNER_COUNT))
     print(str(data))
     SOCKETIO.emit('restart', data, broadcast=True, include_self=False)
 
+
 @SOCKETIO.on('move')
 def on_move(data):
+    """socket event for move, will display updated board to all players/spectators"""
     print(str(data))
     global WINNER_COUNT
     WINNER_COUNT = 0
     SOCKETIO.emit('move', data, broadcast=True, include_self=False)
-    
+
+
 if __name__ == "__main__":
     DB.create_all()
     SOCKETIO.run(
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
+"""Database model for player"""
 from app import DB
 
+
 class Player(DB.Model):
+    """Class of database model imported into app.py"""
     id = DB.Column(DB.Integer, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
     score = DB.Column(DB.Integer, unique=False, nullable=False)
