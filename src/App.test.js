0e4097d5229c5a78fb07c0b24a0f0c62b47d4c78
Jacob White <jjw37@njit.edu>
2021-03-14 15:14:58 +0000

    Added in jest tests for front end testing, on login, board click, and show leaderboard

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import { render,fireEvent, waitFor, screen } from '@testing-library/react';
 import App from './App';
+import Board from './Board'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('removes login after button press', () => {
+  const resultRender = render(<App />);
+  
+  const loginButton= screen.getByText('Login');
+  expect(loginButton).toBeInTheDocument();
+  
+  fireEvent.click(loginButton)
+  
+  expect(loginButton).not.toBeInTheDocument();
+  
+  const gameStatus= screen.getByText('The game is in progress');
+  expect(gameStatus).toBeInTheDocument();
+});
+
+test('restart board', () => {
+  const resultRender = render(<Board />);
+  
+  const restartButton= screen.getByText('Restart Game');
+  const xBox = screen.getAllByText('').forEach(node => fireEvent.click(node))
+  const xText = screen.getAllByText('X').forEach(node =>expect(node).toBeInTheDocument());
+  
+  
+  fireEvent.click(restartButton)
+    
+  const nullText = screen.getAllByText('').forEach(node =>expect(node).toBeInTheDocument());
+});
+
+test('render leaderBoard after login in and pressing show leaderBoardButton', () => {
+  const resultRender = render(<App />);
+  
+  const loginButton= screen.getByText('Login');
+  
+  fireEvent.click(loginButton)
+  
+  const leaderButton= screen.getByText('Show/Hide leaderBoard');
+  const preText= screen.getByText('Press button to see scores')
+  expect(preText).toBeInTheDocument();
+  
+  fireEvent.click(leaderButton)
+  const leaderText= screen.getByText('score')
+  
+  expect(leaderText).toBeInTheDocument();
+  expect(preText).not.toBeInTheDocument();
 });
